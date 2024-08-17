3311432628c447464092351bb9ca4a0836ff55b5
Mateusz Gieroba <Mateusin26@gmail.com>
2024-08-17 21:00:57 +0200

    Properly export AirportChooser

--- flycierge/site/src/components.ts
+++ flycierge/site/src/components.ts
@@ -2,5 +2,5 @@ export * from 'components/SearchPanel';
 export * from 'components/TopBar';
 export * from 'components/ItineraryPanel';
 export * from 'components/ItineraryCard';
-
 export * from 'components/DataFetcher';
+export * from 'components/AirportChooser';
