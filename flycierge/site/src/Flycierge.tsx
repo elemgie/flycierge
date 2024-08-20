af0e49a0a092d3598d3aaa224744733db9a1ef2d
Mateusz Gieroba <Mateusin26@gmail.com>
2024-08-20 02:59:45 +0200

    Add working filtering mechanism

--- flycierge/site/src/Flycierge.tsx
+++ flycierge/site/src/Flycierge.tsx
@@ -1,11 +1,11 @@
-import  { useState } from 'react';
+import  { useCallback, useState, useMemo } from 'react';
 import { tss } from 'tss-react';
 
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 
 import { SearchPanel, TopBar, ItineraryPanel, PriceMetricPanel, FilterPanel } from 'components';
-import { SearchResult } from 'models';
+import { SearchResult, Itinerary } from 'models';
 
 import moment from 'moment';
 import 'moment/locale/en-gb';
@@ -50,6 +50,48 @@ function Flycierge() {
 
   const [searchResult, setSearchResult] = useState<SearchResult | null>(null);
   const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [maxPrice, setMaxPrice] = useState<number>(Number.MAX_VALUE);
+  const [maxFlightNumberString, setMaxFlightNumberString] = useState<string>('any');
+  const [sorterChoice, setSorterChoice] = useState<string>('cheapest');
+
+  const filterFunc = useCallback((itinerary: Itinerary) => {
+    const maxFlightNumber = maxFlightNumberString === 'any' ?
+        Number.MAX_VALUE : Number.parseInt(maxFlightNumberString);
+    return itinerary.outboundFlights.length <= maxFlightNumber &&
+        itinerary.returnFlights.length <= maxFlightNumber &&
+        itinerary.price.value <= maxPrice;
+  }, [maxFlightNumberString, maxPrice]);
+
+  const priceSorter = useCallback((a: Itinerary, b: Itinerary) =>
+    a.price.value - b.price.value,
+[]);
+
+const earliestDepartureTimeSorter = useCallback((a: Itinerary, b: Itinerary) =>
+    a.outboundFlights[0].startDateTime.diff(b.outboundFlights[0].startDateTime),
+[]);
+
+const latestDepartureTimeSorter = useCallback((a: Itinerary, b: Itinerary) =>
+    earliestDepartureTimeSorter(b, a), [earliestDepartureTimeSorter]);
+
+const sorterFunc = useMemo(() => {
+    switch (sorterChoice) {
+        case 'earliest':
+            return earliestDepartureTimeSorter;
+        case 'latest':
+            return latestDepartureTimeSorter;
+        default:
+            return priceSorter;
+    }
+}, [sorterChoice, earliestDepartureTimeSorter, latestDepartureTimeSorter, priceSorter]);
+
+  const processedSearchResult = useMemo(() => {
+    if (!filterFunc || !sorterFunc || !searchResult|| searchResult.itineraries.length === 0) {
+      return searchResult;
+    }
+
+    const processedItineraries = searchResult.itineraries.filter(filterFunc).sort(sorterFunc);
+    return { ...searchResult, itineraries: processedItineraries };
+  }, [searchResult, sorterFunc, filterFunc]);
 
   return (
     <Grid className={classes.mainContainer}>
@@ -61,9 +103,18 @@ function Flycierge() {
       </div>
       <Stack className={classes.contentBox} direction='row'>
         <div className={classes.filterPanel}>
-          <FilterPanel />
+        {searchResult && searchResult.itineraries.length > 0 &&
+          <FilterPanel
+            maxPrice={maxPrice}
+            setMaxPrice={setMaxPrice}
+            maxFlightNumberString={maxFlightNumberString}
+            setMaxFlightNumberString={setMaxFlightNumberString}
+            sorterChoice={sorterChoice}
+            setSorterChoice={setSorterChoice}
+            />
+        }
         </div>
-        <ItineraryPanel itineraries={searchResult?.itineraries || null} isFetching={isFetching} showFullDates/>
+        <ItineraryPanel itineraries={processedSearchResult?.itineraries || null} isFetching={isFetching} showFullDates/>
         <div className={classes.priceMetricPanel}>
           <PriceMetricPanel searchResult={searchResult} />
         </div>
--- flycierge/site/src/components/FilterPanel.tsx
+++ flycierge/site/src/components/FilterPanel.tsx
@@ -1,29 +1,88 @@
-import React, {} from 'react';
+import React, { useState, useCallback, useMemo, useEffect, ChangeEvent } from 'react';
 import { tss } from 'tss-react';
 
+import { Itinerary } from 'models';
+
 import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
 
 const useStyles = tss.create({
     mainPanel: {
         marginRight: 24,
         marginTop: 8,
-        justifySelf: 'center',
+        padding: 8,
         borderColor: 'grey',
         borderStyle: 'solid',
         borderWidth: 1,
         borderRadius: 10,
         display: 'flex',
-        maxHeight: '20vh',
-        width: '20vw',
+        flexDirection: 'column',
+        width: '15vw',
+        height: '50vh',
         backgroundColor: '#F1F1F1'
     }
 })
 
-function PureFilterPanel() {
+interface FilterPanelProps {
+    // setFilter: (filterFunc: (itinerary: Itinerary) => boolean) => void;
+    // setSorter: (sorterFunc: (a: Itinerary, b: Itinerary) => number) => void;
+    setMaxPrice: (val: number) => void;
+    setMaxFlightNumberString: (val: string) => void;
+    setSorterChoice: (val: string) => void;
+    maxPrice: number;
+    maxFlightNumberString: string;
+    sorterChoice: string;
+}
+
+function PureFilterPanel({ maxPrice, maxFlightNumberString, sorterChoice, setMaxPrice, setMaxFlightNumberString, setSorterChoice }: FilterPanelProps) {
     const { classes } = useStyles();
 
+    const handleMaxFlightNumberChange = useCallback((_event: ChangeEvent<HTMLInputElement>, val: string) =>
+        setMaxFlightNumberString(val), []);
+
+    const handleMaxPriceChange = useCallback((_event: any, val: number | number[]) =>
+        setMaxPrice(val as number), []);
+
+    const handleSortertChoiceChange = useCallback((_event: ChangeEvent<HTMLInputElement>, val: string) => setSorterChoice(val), []);
+
     return <Box className={classes.mainPanel}>
-    </Box>
+        <Typography fontWeight='bold' marginBottom={1}>Filters</Typography>
+        <FormControl>
+            <FormLabel>Max number of flights on one way</FormLabel>
+            <RadioGroup
+                value={maxFlightNumberString}
+                onChange={handleMaxFlightNumberChange}
+            >
+                <FormControlLabel value='any' control={<Radio />} label="Any" />
+                <FormControlLabel value='1' control={<Radio />} label="Direct" />
+                <FormControlLabel value='2' control={<Radio />} label="2" />
+                <FormControlLabel value='3' control={<Radio />} label="3" />
+            </RadioGroup>
+        </FormControl>
+        <FormControl>
+            <FormLabel>Max price</FormLabel>
+            <Slider value={maxPrice} step={1} max={10000} onChange={handleMaxPriceChange} size='small' valueLabelDisplay="auto"/>
+        </FormControl>
+        <Divider orientation='horizontal' sx={{marginTop: 1, marginBottom: 1}}/>
+        <FormControl>
+            <FormLabel>Sort flights</FormLabel>
+            <RadioGroup
+                value={sorterChoice}
+                onChange={handleSortertChoiceChange}
+            >
+                <FormControlLabel value='cheapest' control={<Radio />} label="Cheapest" />
+                <FormControlLabel value='earliest' control={<Radio />} label="Earliest" />
+                <FormControlLabel value='latest' control={<Radio />} label="Latest" />
+            </RadioGroup>
+        </FormControl>
+    </Box>;
 }
 
 export const FilterPanel = React.memo(PureFilterPanel);
